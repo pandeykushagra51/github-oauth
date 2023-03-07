4552dbf8331b84baa7be9c789a90ed6a1c5aad08
kushagrapandey <kushagra.pandey@bikayi.com>
2023-03-07 16:52:17 +0530

    added github access for localhost

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 export const config = {
-  GITHUB_CLIENT_ID: 'e6091bdbec940c89e8cb',
-  GITHUB_CLIENT_SECRET: '3828704b49c265beb486f9ce78367d74a7b75c00',
+  GITHUB_CLIENT_ID: 'bbb206598406008f6cc3',
+  GITHUB_CLIENT_SECRET: '3e20cf0ce484331bf6912e95e5cc6b6daf8dfd99',
   TEMPLATE_REPO: 'C-Server',
   TEMPLATE_OWNER: 'pandeykushagra51'
 }
